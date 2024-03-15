211d3398e6703958bb41e275ed3069e45ee7ac77
Gabriel Costa Soares <119671456+gcostasoares@users.noreply.github.com>
2024-03-15 18:44:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ navContainer.addEventListener('click', function() {
 const backgroundText = document.querySelector('.background-text');
 
 window.addEventListener('scroll', function() {
-    var scrollRatio = 0.2;
+    var scrollRatio = 0.5;
     const scrollFactor = (window.scrollY*scrollRatio) + 'px'; 
     backgroundText.style.setProperty('--scrollFactor', scrollFactor);
 
